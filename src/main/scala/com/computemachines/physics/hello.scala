3e525634eb31be6e45fdbbc81ded1283ced694af
Tyler Parker <computemachines@gmail.com>
2016-11-27 18:12:52 -0500

    Add hello world lwjgl opengl window

--- src/main/scala/com/computemachines/physics/hello.scala
+++ src/main/scala/com/computemachines/physics/hello.scala
@@ -1,5 +1,40 @@
 package com.computemachines.physics;
 
-class Hello {
-  def f = "hello worlds"
+import org.lwjgl._
+import org.lwjgl.glfw._
+import org.lwjgl.glfw.GLFW._
+import org.lwjgl.opengl._
+import org.lwjgl.opengl.GL11._
+
+import scala.Null
+
+object Hello extends App{
+  glfwInit()
+  
+  val window: Long = glfwCreateWindow(400, 400, "Hello worlds",
+    null.asInstanceOf[Int], null.asInstanceOf[Int])
+  glfwMakeContextCurrent(window)
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(1);
+
+  
+  glfwSetKeyCallback(window,
+    new GLFWKeyCallbackI {
+      override def invoke(window: Long, key: Int, scancode: Int,
+        action: Int, mods: Int) = {
+        if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+          glfwSetWindowShouldClose(window, true)
+      }
+    })
+  
+  GL.createCapabilities();
+  glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window)
+    glfwPollEvents()
+  }
+
+  glfwTerminate()
+  println("done")
 }
